e139a61dff5cb15d982eab76d4a5db82e2e31638
Josiah Donor <jdonor@gmail.com>
2018-02-06 15:52:15 -0600

    Add Book title= method

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def title= title
+        conjunctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+        prepositions = ["at", "by", "for", "in", "of", "on", "to", "up"]
+        articles = ["a", "the", "an"]
+        @title = title.split(" ").map.with_index do |word, index|
+            unless index > 0 and (conjunctions.include? word or prepositions.include? word or articles.include? word)
+                word.capitalize
+            else
+                word
+            end
+        end .join(" ")
+    end
 end
