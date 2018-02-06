87eba3adebd497314881ba8f271aff32db5e8444
Josiah Donor <jdonor@gmail.com>
2018-02-06 15:38:34 -0600

    Add translate method
    Along with translate_one and add_ay_to helper methods

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+def translate input
+    input.split(" ").map do |word|
+        translate_one word
+    end .join(" ")
+end
+
+def translate_one word
+    first_vowel_index = /[aeio]|!qu/i =~ word
+    if first_vowel_index == 0
+        add_ay_to word
+    else
+        add_ay_to(word[first_vowel_index..-1] + word[0..first_vowel_index-1])
+    end
+end
+
+def add_ay_to input
+    input + "ay"
+end
