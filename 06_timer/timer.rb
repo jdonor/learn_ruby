52d4cc86be567a3b72c8d502d7cc9560a7148eb9
Josiah Donor <jdonor@gmail.com>
2018-02-06 15:59:37 -0600

    Implement Timer class
    Methods: initialize, time_string

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    minutes = @seconds / 60
+    @seconds %= 60
+    hours = minutes / 60
+    minutes %= 60
+    "%02d:%02d:%02d" % [hours, minutes, @seconds]
+  end
 end
