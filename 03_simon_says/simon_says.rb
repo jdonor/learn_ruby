ff1141e00c78559409d1e9719a3ce03dd82c8a9f
Josiah Donor <jdonor@gmail.com>
2018-02-06 14:43:46 -0600

    Add repeat method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,3 +6,12 @@ end
 def shout input
     input.upcase
 end
+
+def repeat input, repeats = 2
+    output = []
+    repeats.times do
+        output.push input
+    end
+    
+    output.join " "
+end
